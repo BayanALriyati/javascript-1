308ad8ba3fee1fd5ca97562931917efd3bcdb5ac
bayanalriyati <bayanalriyati95@gmail.com>
2022-11-05 15:46:36 +0200

    tasks

---
+++ java/task1.js
@@ -0,0 +1,15 @@
+  let num1=prompt ('x');
+  let  num2=prompt ('y');
+if  (num1 > num2) {
+    alert("Hello World");
+    console.log ("Hello World");
+    let promptMsg = prompt(x .y)
+} 
+    else
+{
+    alert("Goodbye");
+    console.log ("Goodbye");
+    prompt ("Goodbye");
+}   
+
+
---
+++ java/task2.js
@@ -0,0 +1,19 @@
+let role=prompt ('*,-,+,/');
+let num1=prompt ('num1');
+let  num2=prompt ('num2');
+if ( role== "+"){
+    console.log (num1 + num2) ;
+    document.write (num1 + num2) ;
+}
+else if ( role== "-"){
+    console.log (num1 - num2) ;
+    document.write (num1 - num2) ;
+}
+else if (role== "*"){
+    console.log (num1 * num2) ;
+    document.write (num1 * num2) ;
+} 
+else if ( role== "/"){
+    console.log (num1 / num2) ;
+    document.write (num1 / num2) ;
+}
