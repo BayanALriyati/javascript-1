278bed28f8877fd99fd41273a436ee6048580725
bayanalriyati <bayanalriyati95@gmail.com>
2022-11-07 21:26:32 +0200

    tasks

--- Monday_7_11/tast1.js
+++ Monday_7_11/tast1.js
@@ -1,185 +1,185 @@
 
-// 1
-// function tellFortune(jobTitle, geoLocation, partners, children) {
-//     var future = 'You will be a ' + jobTitle + ' in ' + geoLocation + ' and married to ' +
-//    partners + ' ' + ' with ' + children + 'children .';
-//     console.log(future);
-// }
+1
+function tellFortune(jobTitle, geoLocation, partners, children) {
+    var future = 'You will be a ' + jobTitle + ' in ' + geoLocation + ' and married to ' +
+   partners + ' ' + ' with ' + children + 'children .';
+    console.log(future);
+}
 
-// tellFortune('engineer', 'aqaba', 'noor', 3);
-// tellFortune('doctor', 'kark', 'fatmia', 7);
-// tellFortune('teacher', 'amman', 'ashwaq', 0);
-
-
-// 
-
-// 2
-// function calculateDogAge( age) {
-//     var result= 7*age ;
-//     console.log('Your doggie is '  + result +  ' years old in dog');
-// }
-
-// calculateDogAge(3);
-// calculateDogAge(7);
-// calculateDogAge(8);
+tellFortune('engineer', 'aqaba', 'noor', 3);
+tellFortune('doctor', 'kark', 'fatmia', 7);
+tellFortune('teacher', 'amman', 'ashwaq', 0);
 
 
-// 
+// // 
 
-// 3
-// function calculateSupply ( age , amount) {
-//     var maxage= 100;
-//     var result=(amount * 365) * (maxage - age)
-//     console.log('You will need '  + result +  ' cups of tea to last you until the ripe old age of 100');
-// }
+2
+function calculateDogAge( age) {
+    var result= 7*age ;
+    console.log('Your doggie is '  + result +  ' years old in dog');
+}
 
-// calculateSupply (27 , 6);
-// calculateSupply (70 , 6);
-
-
-// 
-
-// 4
-// function greet ( name) {
-//      var greet='Hello ' + name
-//      console.log( greet );
-// }
-
-// greet ("Bayan");
-// greet ("Ahmad");
+calculateDogAge(3);
+calculateDogAge(7);
+calculateDogAge(8);
 
 
-// 
+// // 
 
-// 5
-// function double(i) {
-//     return 2 * i;
-//   }
+3
+function calculateSupply ( age , amount) {
+    var maxage= 100;
+    var result=(amount * 365) * (maxage - age)
+    console.log('You will need '  + result +  ' cups of tea to last you until the ripe old age of 100');
+}
+
+calculateSupply (27 , 6);
+calculateSupply (70 , 6);
+
+
+// // 
+
+4
+function greet ( name) {
+     var greet='Hello ' + name
+     console.log( greet );
+}
+
+greet ("Bayan");
+greet ("Ahmad");
+
+
+// // 
+
+5
+function double(i) {
+    return 2 * i;
+  }
   
-//   function double(a) {
-//     return 2 * a;
-//   }
-//   double(7)
+  function double(a) {
+    return 2 * a;
+  }
+  double(7)
   
-//   function double(i) {
-//     return 2 * i;
+  function double(i) {
+    return 2 * i;
    
-//   }
-//   double()
+  }
+  double()
 
 
-// 
+// // 
 
-6
-
-
+// 6
 
 
-// 7
-// function cube( number ) {
-//     var ruslt= (Math.pow(number,3)) ;
-//     console.log(ruslt);
-// }
-
-// cube(3);
-// cube(7);
 
 
-// 
+7
+function cube( number ) {
+    var ruslt= (Math.pow(number,3)) ;
+    console.log(ruslt);
+}
 
-// 8
-// function multiplyBy(num1,num2)
-// {
-//         result=num1*num2
-//         console.log(result);
-// }   
-// multiplyBy(prompt(),prompt())
-// multiplyBy(100,50)
-// multiplyBy(10,2)
+cube(3);
+cube(7);
 
 
-// 
+// // 
 
-// 9
-// function canIGetADrivingLicense (age) {
-
-//     if (age >= 20){
-
-//         console.log(`yes you can`)
-//     }
-//     else if (age < 20) {
-
-
-//         i= 20-age
-//         console.log(`please come back after ${i} years to get one`)
-//     }
-
-// }
-// canIGetADrivingLicense(prompt())
+8
+function multiplyBy(num1,num2)
+{
+        result=num1*num2
+        console.log(result);
+}   
+multiplyBy(prompt(),prompt())
+multiplyBy(100,50)
+multiplyBy(10,2)
 
 
-// 
+// // 
 
-// 10
+9
+function canIGetADrivingLicense (age) {
 
-// function sameLength (a , b) {
+    if (age >= 20){
 
-//     if (a.length == b.length){
-
-//         console.log(`True`)
-//     }
-//     else {
-//         console.log(`False`)
-//     }
-
-// }
-// sameLength(prompt() , prompt())
-// sameLength('tree' , 'clue')
+        console.log(`yes you can`)
+    }
+    else if (age < 20) {
 
 
-// 
+        i= 20-age
+        console.log(`please come back after ${i} years to get one`)
+    }
 
-// 11
-
-// function largerNubmer (number1 , number2) {
-
-//     if (number1 > number2){
-
-//         console.log(number1)
-//     }
-//     else {
-//         console.log(number2)
-//     }
-
-// }
-// largerNubmer(prompt() , prompt())
-// largerNubmer( 1 , 2)
+}
+canIGetADrivingLicense(prompt())
 
 
-// 
+// // 
 
-// 12
+10
 
-// function smallerNubmer (number1 , number2 ,number3) {
+function sameLength (a , b) {
 
-//     if ((number1 < number2) && (number1 < number1)){
+    if (a.length == b.length){
 
-//         console.log(number1)
-//     }
-//     else if ((number2 < number3) && (number2 < number1))
-//         {
-//         console.log(number2)
-//     }
-//     else
-//         console.log(number3)
+        console.log(`True`)
+    }
+    else {
+        console.log(`False`)
+    }
+
+}
+sameLength(prompt() , prompt())
+sameLength('tree' , 'clue')
+
+
+// // 
+
+11
+
+function largerNubmer (number1 , number2) {
+
+    if (number1 > number2){
+
+        console.log(number1)
+    }
+    else {
+        console.log(number2)
+    }
+
+}
+largerNubmer(prompt() , prompt())
+largerNubmer( 1 , 2)
+
+
+// // 
+
+12
+
+function smallerNubmer (number1 , number2 ,number3) {
+
+    if ((number1 < number2) && (number1 < number1)){
+
+        console.log(number1)
+    }
+    else if ((number2 < number3) && (number2 < number1))
+        {
+        console.log(number2)
+    }
+    else
+        console.log(number3)
     
 
-// }
-// smallerNubmer(prompt() , prompt() , prompt())
-// smallerNubmer( 20 , 10 , 8)
+}
+smallerNubmer(prompt() , prompt() , prompt())
+smallerNubmer( 20 , 10 , 8)
 
 
-// 
+// // 
 
 13
 
