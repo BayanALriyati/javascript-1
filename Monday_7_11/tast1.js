5737385e84b5a12ef7d5f365e422afa4c862ba74
bayanalriyati <bayanalriyati95@gmail.com>
2022-11-08 22:58:23 +0200

    tasks

--- Monday_7_11/tast1.js
+++ Monday_7_11/tast1.js
@@ -183,6 +183,38 @@ smallerNubmer( 20 , 10 , 8)
 
 // // 
 
-// 13
+// 14
+
+// Ex: longerString("air","school","car","by","github")
+
+let a="air";
+let b="school";
+let c="car";
+let d="by";
+let e="github";
+function longerString(a,b,c,d,e){
+if ((a.length > b.length) &&  (a.length > c.length) && (a.length > d.length) && (a.length > e.length)){
+    console.log (a)
+}
+else if ((b.length > a.length) && (b.length > c.length) && (b.length > d.length) && (a.length > e.length)){
+    console.log (b)
+}
+else if ((c.length > a.length) && (c.length > b.length) && (c.length > d.length) && (c.length > e.length)){
+    console.log (c)
+}
+else if ((d.length > a.length) && (d.length > b.length) && (d.length > c.length) && (d.length > e.length)){
+    console.log (d)
+}
+else {
+    console.log (e)
+}
+}
+longerString(a,b,c,d,e)
+
+
+
+
+
+
 
 
---
+++ Tuesday_8_11/task1.js
@@ -0,0 +1,107 @@
+// // 1 =Correct the syntax error
+//   let a=[1, 7 , 9 , 45,]
+//   let b= ["Str" , "alex" , "moh" , "the" ,"fox" , "over" , "lazy" , "dog"  ]
+//     console.log(a , b);
+
+
+// 2 =What the index of "Banana","Tomato"
+//   let fruits=["Tomato","Banana","Watermelon"];   
+//   let index = fruits.indexOf("Tomato");  
+//   console.log(index);
+//   let index1 = fruits.indexOf("Banana");  
+//   console.log(index1);
+ 
+
+// 3 =Create an array represents your :Favorite Food 
+// let Food=["Apple" , "Orange" , "Banana" , "Grape" , "Blackberries"]
+// let Sport=["Tennis" , "Football" , "Cricket"]
+// let Movie=["Action" , "Drama" , "Black Adam" , "Barbarian"]
+// console.log(Food);
+// console.log(Sport);
+// console.log(Movie);
+
+
+//  4 =Create a function called firstOfArray that take an array as a parameter and return the first element in an array
+// let a=[1,4,5]
+// let b=["t","u","g","x"]
+// function firstOfArray(a,b){
+//     console.log(a[0]);
+//     console.log(b[0]);
+
+// }
+// firstOfArray(a,b);
+
+
+//  5 =Create a function called lastOfArray that take an array as a parameter and return the first element in an array 
+// let a=[1,4,5]
+// let b=["t","u","g","x"]
+// function lastOfArray(a,b){
+//     console.log(a[2]);
+//     console.log(b[3]);
+
+// }
+// lastOfArray(a,b);
+
+
+// /6  =Using console make this array to be like this one (push, unshift, shift, pop) [1,3,4,6,8,9,10]
+// //     
+//     let array = [0,5,7,9]
+//     let array1=array.push(10);
+//     let array2=array.shift();
+//     let array3=array.shift();
+//     let array4=array.shift();
+//     let array5=array.unshift(1,3,4,6,8)
+//     console.log (array)
+
+
+//   7  =Using the console try to figure out what the thing thats (push, unshift, shift, pop) return to you
+    // let array = [5,9,-7,3.5]
+    // let array1=array.push(50);
+    // console.log(array);
+    // let array2=array.unshift(100);
+    // console.log(array);
+    // let array3=array.shift();
+    // console.log(array);
+    // let array5=array.pop();
+    // console.log(array);
+    
+
+
+
+//   8  =Create a function called middleOfArray that take an array as a parameter and return the middle element in an array if it is have an odd elemnets and return the two middle elemnt in an array if it is have an even elemnets
+
+//     function middleOfArray (array) {
+//   if ( array.length % 2 == 0){
+//     return array.splice(array.length/2 - 1 , array.length/2 )
+//   }
+//   else {
+//     return array[Math.floor(array.length/2)]
+//   }
+// }
+
+// console.log(middleOfArray(["a","b","c","d","f"]));
+// console.log(middleOfArray(["1","2","3","4"]));
+
+
+//  
+// //   11 =Create a function called arrayExceptLast that accept an array and return the entire array except the last elemnt
+    // let nums=[1,2,3,8,9]
+    // let num=nums.pop();
+
+    // function arrayExceptLast(){
+    //     console.log(nums);
+    // }
+    // arrayExceptLast()
+
+
+//    12 =Create a function called addToEnd that accept an array and value and return the entire array with add this value to the end of this array
+// let nums=[1,2,3,8,9]
+// let num=nums.push(55);
+
+
+// function addToEnd(){
+//     console.log(nums);
+// }
+// addToEnd()
+     
+ 
